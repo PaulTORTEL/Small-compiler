dbf922490a785fa781d833e5d9b7fca26105430a
Vincent <vincent.coursac@gmail.com>
2017-03-03 15:40:38 +0100

    Fin de resolution du merge

--- LC/src/Analyser.h
+++ LC/src/Analyser.h
@@ -9,16 +9,14 @@
 
 class Analyser
 {
-    std::string _startSymbol;
 
     private:
 
-<<<<<<< HEAD
-    /*std::map<std::string, std::vector<std::string> > _grammar;*/
+
     std::string _startSymbol;
-=======
+
     std::map<std::string, Symbol*> _grammar;
->>>>>>> 9d5c61046abe3a6b0db36618536a9403cb2a84ac
+
 
     public:
         Analyser();
